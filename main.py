0b82121cf9eb148f4b36b0cc1abc10d627f3802a
Samantha Noggle <44234583+astruxie@users.noreply.github.com>
2022-01-05 22:55:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-# Program that searches a file for a user-specified word
-# returning all the words with maximum 2 total
+# -----------------------------------------------------------
+# Program that searches a file for a user-specified word &
+# returns all the words with maximum 2 total
 # insertions and deletions (in no particular order)
+#
+# Samantha Noggle
+# -----------------------------------------------------------
 from timeit import default_timer as timer
 
-def longest_common_subsequence(str1, str2, m, n):
+def longest_common_subsequence(str1, str2, x, y):
  
-    L = [[0 for i in range(n + 1)]
-         for i in range(m + 1)]
+    L = [[0 for i in range(y + 1)]
+         for i in range(x + 1)]
  
-    # Following steps build L[m+1][n+1]
-    # in bottom up fashion. Note that
-    # L[i][j] contains length of LCS
-    # of str1[0..i-1] and str2[0..j-1]
-    for i in range(m + 1):
-        for j in range(n + 1):
+    # Build the grid to compare each letter in the two words
+    # L[i][j] contains length of LCS of string1[0...i-1] and string[0...j-1]
+    # L[x][y] contains the length of the longest common subsequnece
+    for i in range(x + 1):
+        for j in range(y + 1):
             if (i == 0 or j == 0):
                 L[i][j] = 0
             elif(str1[i - 1] == str2[j - 1]):
@@ -22,28 +25,27 @@ def longest_common_subsequence(str1, str2, m, n):
                 L[i][j] = max(L[i - 1][j],
                               L[i][j - 1])
  
-    # Return length of LCS
-    return L[m][n]
+    # Return length of longest common subsequence
+    return L[x][y]
 
 def isWordWithinRange(str1, str2):
     leng = longest_common_subsequence(str1, str2, len(str1), len(str2))
     deletions = len(str1) - leng
     insertions = len(str2) - leng
-    total = insertions + deletions
-    if total > 2:
+
+    if (insertions + deletions) > 2:
         return False
     else:
         return True
 
-# Variables
+# Driver code
+
 similarWords = []
-# Get the parameters
+
 input = input("Please enter your word to search for: ")
 
-# Open file to start the search
 f = open("english_words.txt", "r").read().splitlines()
 
-# Timing the search
 start = timer()
 
 # Check each word in the file & add it to the list if 
@@ -54,7 +56,6 @@ for word in f:
         if isWordWithinRange(word, input):
             similarWords.append(word)
 
-# Timing the search
 end = timer()
 
 # Print results in no particular order with duplicates
